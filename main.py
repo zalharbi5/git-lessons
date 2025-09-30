b8f74626a8cd3adc746c432b47a839b4af1a8974
zalharbi5 <zalharbi789@gmail.com>
2025-09-30 14:23:29 +0300

    Starting branches lesson

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 print("Hello version control")
+
 """
 To config:
 git config --global user.name "your name"
@@ -15,7 +16,15 @@ git config user.email
 # git add . == (adds every file in current dir to repo)
 # git add exm.py exm2.py exm3.py == add multiple files to repo
 # git commit -m "what you have done" == update with comment stating what you did
-# always when you modify anaything do git add then git commit
+# always when you modify anaything do (add) then (commit)
+# git status == show the current status (tells you if anything is modified but not uploaded to repo)
 # git log == show all previous commits
-# ^^ to exit git log, enter 'q'
+"""
+For git log command:
+Scroll down: Use j or the down arrow key.
+Scroll up: Use k or the up arrow key.
+Page down: Use Spacebar or Page Down.
+Page up: Use b or Page Up.
+Quit the pager: Use q.
+"""
 # git commit --amend -m "your modified comment" == moddify a commit comment that you passed
