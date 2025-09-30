6d0e2fe88e5bb2b25a405199e694d7b40723bcd1
zalharbi5 <zalharbi789@gmail.com>
2025-09-30 13:54:17 +0300

    new comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 print("Hello version control")
-# get add . == (adds every file in dir)
-# get add exm.py exm2.py exm3.py == add multiple file
+# git init == make current dir a repo in git (git initialize)
+# git add . == (adds every file in dir to repo)
+# git add exm.py exm2.py exm3.py == add multiple file
 # git commit -m "what you have done" == update with comment
 # git log == show all previous commits
+# ^^ to exit git log, enter 'q'
 # always when you modify anaything do git add then git commit
