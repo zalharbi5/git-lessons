7d670e0d8461016ccce25f3e9903bdb648b27208
zalharbi5 <zalharbi789@gmail.com>
2025-09-30 14:02:01 +0300

    Even more comments 2:01

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 print("Hello version control")
-# git init == make current dir a repo in git (git initialize)
-# git add . == (adds every file in dir to repo)
-# git add exm.py exm2.py exm3.py == add multiple file
-# git commit -m "what you have done" == update with comment
+"""
+To config:
+git config --global user.name "your name"
+git config --global user.email "your email"
+
+To check your current config:
+git config user.name
+git config user.email
+"""
+
+# cls == clear current terminal screen (windows)
+# clear == clear current terminal screen (linux)
+# git init == make current dir a repo in git (git initialization)
+# git add . == (adds every file in current dir to repo)
+# git add exm.py exm2.py exm3.py == add multiple files to repo
+# git commit -m "what you have done" == update with comment stating what you did
+# always when you modify anaything do git add then git commit
 # git log == show all previous commits
 # ^^ to exit git log, enter 'q'
-# always when you modify anaything do git add then git commit
+# git commit --amend -m "your modified comment" == moddify a commit comment that you passed
