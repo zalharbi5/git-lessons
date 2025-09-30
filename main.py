0649e60bdc6e0bbea1b0f7205e9267dcf60a0b44
zalharbi5 <zalharbi789@gmail.com>
2025-09-30 14:28:27 +0300

    test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print("Hello version control")
-
+print("Welcome all!")
+print("New print")
 """
 To config:
 git config --global user.name "your name"
@@ -16,7 +17,7 @@ git config user.email
 # git add . == (adds every file in current dir to repo)
 # git add exm.py exm2.py exm3.py == add multiple files to repo
 # git commit -m "what you have done" == update with comment stating what you did
-# always when you modify anaything do (add) then (commit)
+# ^^ always when you modify anaything do (add) then (commit)
 # git status == show the current status (tells you if anything is modified but not uploaded to repo)
 # git log == show all previous commits
 """
@@ -28,3 +29,14 @@ Page up: Use b or Page Up.
 Quit the pager: Use q.
 """
 # git commit --amend -m "your modified comment" == moddify a commit comment that you passed
+# git branch branch-name == create new branch
+# git branch == list the branches you have created
+# ^^ the green colored branch is the current branch you are working on
+# git checkout branch-name == moves you to the specified branch
+# git checkout -b branch-name = create a new branch then move to it (2 commands in 1)
+
+# to merge sub-branch with master-branch:
+"""
+1- move to master-branch (git checkout master)
+2- 
+"""
