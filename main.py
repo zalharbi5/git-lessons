60cf643f08d9ef6bf1867254c607e07ac7f90a5d
zalharbi5 <zalharbi789@gmail.com>
2025-09-30 13:43:32 +0300

    Updated string to (Hello version control)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("hello world")
-# get add . (adds every file in dir)
-# get add exm.py exm2.py exm3.py
-# git commint -m "what you have done"
+print("Hello version control")
+# get add . == (adds every file in dir)
+# get add exm.py exm2.py exm3.py == add multiple file
+# git commit -m "what you have done" == update with comment
+# git log == show all previous commits
+# always when you modify anaything do git add then git commit
